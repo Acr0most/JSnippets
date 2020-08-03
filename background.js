b754dfd6f793ef1d4b2a008a901a8ea4443aa11b
acr0most <do-while-true@gmx.de>
2020-08-03 10:24:27 +0200

    remove dummy snippets - only add gitlab

--- background.js
+++ background.js
@@ -1,14 +1,41 @@
 chrome.runtime.onInstalled.addListener(function () {
-  // chrome.storage.sync.clear();
+ chrome.storage.local.clear();
 
-  chrome.storage.sync.set({
+ chrome.storage.local.set({
     snippet1: {
-      name: "Green",
-      cmd: "document.querySelector('body').style.backgroundColor = 'green'"
+      name: "Gitlab",
+      cmd: "setTimeout(() => document.querySelector(\".diff-files-holder.container-limited.limit-container-width.mx-lg-auto.px-3\").style.maxWidth=\"none\", 5000);",
+      regex: "gitlab(.*)diffs",
+      autoExecute: true,
     },
-    snippet2: {
-      name: "Red",
-      cmd: "document.querySelector('body').style.backgroundColor = 'red'"
-    },
+
   });
 });
+
+chrome.tabs.onUpdated.addListener(function
+  (tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+	console.log(tabId, changeInfo, tab);
+
+	chrome.storage.local.get(null, (elements) => {
+		Object.values(elements).forEach(cmd => {
+			if (!cmd.autoExecute) {
+				return
+			}
+
+			var myRe = new RegExp(cmd.regex, "g");
+
+			if (myRe.exec(changeInfo.url)) {
+				console.log("exodus", changeInfo.url, cmd.regex, cmd.cmd);
+				chrome.tabs.executeScript(tab.id, { code: cmd.cmd });
+/*
+				chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+					chrome.tabs.executeScript(tabs[0].id, { code: cmd.cmd });
+				});
+*/
+			}
+		});
+	});
+    }
+  }
+);
