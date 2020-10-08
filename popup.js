6cc91cf6cc53b994bca6d7013defb620483dfe53
Florian Orben <florian.orben@schmetterling.de>
2020-10-08 10:57:12 +0200

    styles fixes, improve some methods

--- popup.js
+++ popup.js
@@ -1,39 +1,24 @@
-var bkg = chrome.extension.getBackgroundPage();
 let id = null
-
-let remove = document.getElementById('remove');
-remove.onclick = deleteSnippet;
-
-let abort = document.getElementById('abort');
-abort.onclick = reset;
-
-let saveElement = document.getElementById('save');
-saveElement.onclick = save;
-
-let execute = document.getElementById('execute');
-execute.onclick = executeCommand;
-
-window.onload = fillView;
 
 function fillView() {
   document.getElementById("outlet").innerHTML = "";
 
   cmd = chrome.storage.local.get(null, (elements) => {
-    Object.keys(elements).forEach(key => document.getElementById("outlet").innerHTML += "<div id=\"" + key + "\" class=\"snippet\">" + elements[key].name + "</div>");
-  })
+    Object.keys(elements).forEach(key => document.getElementById("outlet").innerHTML += `<div id="${key}" class="snippet">${elements[key].name}</div>`);
+  });
 
   cmd = chrome.storage.local.get(null, (elements) => {
     Object.keys(elements).forEach(key => document.getElementById(key).addEventListener("click", showOptions));
-  })
+  });
 }
 
 function selectSnippet(id) {
-  document.querySelectorAll("#outlet div").forEach(el => el.classList.remove("selected"));
+  Array.from(document.querySelectorAll("#outlet div")).forEach(el => el.classList.remove("selected"));
   document.getElementById(id).classList.add("selected");
 }
 
 function reset() {
-  showOptions(null)
+  showOptions(null);
 }
 
 function save() {
@@ -44,7 +29,7 @@ function save() {
       autoExecute: document.getElementById("options-autoexecute").checked,
       regex: document.getElementById("options-regex").innerText
     }
-  })
+  });
 
   fillView();
 }
@@ -55,14 +40,14 @@ function showOptions(event) {
   chrome.storage.local.get(id, (elements) => {
     let ids = Object.keys(elements);
 
-    if (ids.length == 0 || ids[0] == null) {
+    if (ids.length === 0 || ids[0] == null) {
       showInfo("command with id" + id + "not found" + elements + id, "red", 1000);
-      return
+      return;
     }
 
-    printOptions(elements[ids[0]])
-    selectSnippet(id)
-  })
+    printOptions(elements[ids[0]]);
+    selectSnippet(id);
+  });
 }
 
 function showInfo(message, color, timeout) {
@@ -76,7 +61,7 @@ function showInfo(message, color, timeout) {
 }
 
 function executeCommand() {
-  if (id == -1) {
+  if (id === -1) {
     showInfo("invalid id", "red", 1000);
     return null;
   }
@@ -84,22 +69,22 @@ function executeCommand() {
   chrome.storage.local.get(id, (elements) => {
     let ids = Object.keys(elements);
 
-    if (ids.length == 0 || ids[0] == null) {
+    if (ids.length === 0 || ids[0] == null) {
       showInfo("command with id" + id + "not found" + elements + id, "red", 1000);
-      return
+      return;
     }
 
     chrome.tabs.query({
       currentWindow: true,
       active: true
-    }, function (tabs) {
+    }, (tabs) => {
       chrome.tabs.executeScript(tabs[0].id, {
         code: elements[ids[0]].cmd
       });
     });
 
     showInfo("executed", "#00b940", 1000);
-  })
+  });
 }
 
 function printOptions(cmd) {
@@ -112,64 +97,60 @@ function printOptions(cmd) {
 const add = document.getElementById("add");
 
 add.addEventListener('focus', (event) => {
-  if (event.target.innerText == "+") {
+  if (event.target.innerText === "+") {
     event.target.innerText = "";
     event.target.classList.remove("centered");
   }
 });
 
 add.addEventListener('blur', (event) => {
-  if (event.target.innerText == "") {
+  if (event.target.innerText === "") {
     event.target.innerText = "+";
     event.target.classList.add("centered");
   }
 });
 
-add.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    event.preventDefault();
 
-    let newId = getNextId();
-    chrome.storage.local.get(null, (elements) => {
-      let keys = Object.keys(elements).map(el => parseInt(el.replace("snippet", "")));
-      var last = keys.sort((a, b) => a - b)[keys.length - 1];
-
-      newId = "snippet" + (parseInt(last) + 1)
-
-
-      chrome.storage.local.set({
-        [newId]: {
-          name: document.getElementById("add").value,
-          cmd: "",
-          autoExecute: false,
-          regex: ""
-        }
-      })
-
-      fillView();
-      document.getElementById("add").value = ""
-
-    })
+add.addEventListener("keyup", async event => {
+  if (event.keyCode !== 13) {
+      return;
   }
+
+  event.preventDefault();
+
+  let newId = await getNextId();
+
+  chrome.storage.local.set({
+    [newId]: {
+      name: document.getElementById("add").value,
+      cmd: "",
+      autoExecute: false,
+      regex: ""
+    }
+  });
+
+  fillView();
+  add.value = "";
 });
 
 function deleteSnippet() {
-  chrome.storage.local.remove(id);
-  fillView();
-
+	chrome.storage.local.remove(id);
+	fillView();
 }
 
-function getNextId() {
-  let nextId = null;
-
-  chrome.storage.local.get(null, (elements) => {
-    let keys = Object.keys(elements)
-    var last = keys.sort()[keys.length - 1];
-
-    var id = last.replace("snippet", "");
-
-    nextId = "snippet" + (parseInt(id) + 1)
-  })
-
-  return nextId
+async function getNextId() {
+	return new Promise(resolve => {
+        chrome.storage.local.get(null, (elements) => {
+            const keys = Object.keys(elements).map(el => parseInt(el.replace("snippet", "")));
+            const last = keys.sort((a,b) => a-b)[keys.length-1];
+            return resolve(`snippet${parseInt(last + 1)}`);
+        });
+    });
 }
+
+
+document.getElementById('remove').addEventListener('click', deleteSnippet);
+document.getElementById('abort').addEventListener('click', reset);
+document.getElementById('save').addEventListener('click', save);
+document.getElementById('execute').addEventListener('click', executeCommand);
+window.addEventListener('load', fillView);
